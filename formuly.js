6c0f8336ddd92175397d8acf8525ce005867be1b
Puldzian <puldzian@gmail.com>
2018-09-06 16:49:22 +0200

    huj mnie strzeli

--- formuly.js
+++
@@ -1,9 +0,0 @@
-/* global $ */
-
-// Tutaj są schowane wszystkie generatory, które wykorzystują frazy ze slownik.js,
-// a wywołuje je silnik.js
-$(document).ready(function () {
-  var chrzest = function () {
-    
-  }
-})
--- silnik.js
+++ silnik.js
@@ -1,51 +1,18 @@
 /* global $ */
-
 $(document).ready(function () {
   'use strict'
   var danyRok = 960
-  var daneZdanie = 'zdanie testowe'
-  var koniec = 2017
+  var daneZdanieTestowe = 'element testowy'
   var poleTekstowe = document.getElementById('historia')
 
-  // Losuje któryś z N elementów
-  var losuj = function (n) {
-    return Math.floor(Math.random() * (n))
-  }
-
-  var wstawZdanie = function () {
-    var status = danyRok + ' – ' + daneZdanie + '. '
+  // To wydarza się po naciśnięciu przycisku!
+  var generuj = function () {
+    var status = danyRok + ' – ' + daneZdanieTestowe + '. '
     poleTekstowe.append(status)
   }
 
-  var dodajRok = function () {
-    if (danyRok === 960) { // Chrzest Polski
-      danyRok = danyRok + losuj(10)
-    } else if (danyRok < 1200) { // Wczesne średniowiecze
-      danyRok = danyRok + losuj(40) + 1
-    } else if (danyRok >= 1200 && danyRok < 1400) { // Późne średniowiecze
-      danyRok = danyRok + losuj(30) + 1
-    } else if (danyRok >= 1400 && danyRok < 1600) { // Renesans
-      danyRok = danyRok + losuj(25) + 1
-    } else if (danyRok >= 1600 && danyRok < 1800) { // Barok
-      danyRok = danyRok + losuj(20) + 1
-    } else if (danyRok >= 1800 && danyRok < 1914) { // Zabory
-      danyRok = danyRok + losuj(15) + 1
-    } else if (danyRok >= 1914 && danyRok < 1946) { // Wojny i międzywojnie
-      danyRok = danyRok + losuj(10) + 1
-    } else if (danyRok >= 1946 && danyRok < 1989) { // Komunizm
-      danyRok = danyRok + losuj(10) + 1
-    } else if (danyRok >= 1989 && danyRok <= koniec) { // III RP
-      danyRok = danyRok + losuj(3) + 1
-    }
+  // Placeholder, co tu podepniesz, uruchomi się po naciśnięciu 1 zdania
+  var przycisk = function () { // eslint-disable-line no-unused-vars
+    generuj()
   }
-  var generator = function () {
-    if (danyRok < koniec) {
-      dodajRok()
-      wstawZdanie()
-      generator()
-    }
-  }
-
-  // Odpalenie maszyny
-  generator()
 })
